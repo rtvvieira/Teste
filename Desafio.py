0c658f2f915ea5a64ec9ef580be3fecd44b57021
Rogerio <rogeriotavaresvieira@gmail.com>
2024-11-10 19:09:47 -0400

    Desafio1

---
+++ Desafio.py
@@ -0,0 +1,16 @@
+#Desafio Aula
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)  # Foi alterado do texto para numerico 
+
+print('Programa finalizado')
+
+# F9 - marcar umbreakpoint
+# F5 - Abrir o modo debug
+# F10 - Passar sobre um linha, Sem entrar em métodos internos
+# F11 - Passar sobre uma linha e entrar em método interno
